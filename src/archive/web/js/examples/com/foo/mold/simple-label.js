32a2f5e4174fbc1a33135d4b2686456501e0da85
tmillsclare <timothyclare@gmail.com>
2011-06-10 10:45:53 +0800

    Further updating the structure

--- src/archive/web/js/examples/com/foo/mold/simple-label.js
+++
@@ -1,4 +0,0 @@
-
-function (out) {
- out.push('<span', this.domAttrs_(), '>', '<div id="value" style="float:left;">', this.getValue(), '</div>', '<div id="target" style="float:left;cursor: pointer; cursor: hand;height:20px;width:20px;background-color:red;"></div></span>');
-}
