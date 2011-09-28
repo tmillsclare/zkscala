660c8f4d786d8141b61f6bab7f7fe7dca456c0b6
Timothy Clare <timothyclare@gmail.com>
2011-09-28 09:39:31 +0800

    Added Build.scala file to lib instead of sbt

---
+++ lib/project/Build.scala
@@ -0,0 +1,37 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+	val buildOrganization = "zkoss"
+	val buildVersion      = "0.6"
+
+	val buildSettings = Defaults.defaultSettings ++ Seq (
+		name := "zkscala",
+		organization := buildOrganization,
+		version := buildVersion
+  	)
+}
+
+object Resolvers {
+	val zkcerepo = "ZK CE" at "http://mavensync.zkoss.org/maven2"
+}
+
+object Dependencies {
+	val zkce = "org.zkoss.zk" % "zk" % "5.0.8" 
+	val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
+}
+
+object ZKScalaBuild extends Build {
+	import BuildSettings._
+	import Resolvers._
+	import Dependencies._
+
+	lazy val zkscala = Project(
+		"zkscala",
+		file("."),
+		settings = buildSettings ++ Seq(
+			resolvers := Seq(zkcerepo),
+			libraryDependencies ++= Seq(zkce)
+		)
+	)
+}
