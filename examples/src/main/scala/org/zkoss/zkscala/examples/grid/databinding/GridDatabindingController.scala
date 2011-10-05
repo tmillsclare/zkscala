04e51653c1aabafaf284afb17d1f796a1cd78e9b
Timothy Clare <timothyclare@gmail.com>
2011-10-05 09:33:30 +0800

    Updated the autosorting and databinding to separate the data from the main functionality Example is now much more simple

--- examples/src/main/scala/org/zkoss/zkscala/examples/grid/databinding/GridDatabindingController.scala
+++ examples/src/main/scala/org/zkoss/zkscala/examples/grid/databinding/GridDatabindingController.scala
@@ -9,35 +9,10 @@ package org.zkoss.zkscala.examples.grid.databinding {
 
 	class GridDatabindingController extends GenericForwardComposer {
 		
-		val titles = List("Code", "Bugs", "Docs", "Arts")
+		@BeanProperty var selected = ContributorDataProvider.myContributorList.apply(0)
 
-		val defaultContributor = new Contributor("Jon", "Art", titles.apply(0), 123)
-
-		val myContributorList = Seq(
-			defaultContributor,
-			new Contributor("Kaleb", "Leonel", titles.apply(0), 321),
-		    new Contributor("Balu", "Haben", titles.apply(0), 321),
-		    new Contributor("Trey", "Wyatt", titles.apply(0), 323),
-		    new Contributor("Balu", "Haben", titles.apply(0), 324),
-		    new Contributor("Terry", "Tornado", titles.apply(0), 711),
-		    new Contributor("Jesse", "Miles", titles.apply(1), 712),
-		    new Contributor("Sadira", "Jobs", titles.apply(1), 713),
-		    new Contributor("Jaquan", "Frederick", titles.apply(2), 451),
-		    new Contributor("Avery", "Katrina", titles.apply(2), 453),
-		    new Contributor("Heidi", "Nikolas", titles.apply(2), 455),
-		    new Contributor("Katelyn", "Clara", titles.apply(2), 457),
-		    new Contributor("Branden", "Shane", titles.apply(2), 459),
-		    new Contributor("Dacey", "Obert", titles.apply(2), 450),
-		    new Contributor("Julianna", "Allison", titles.apply(3), 643),
-		    new Contributor("Rachel", "Elisabeth", titles.apply(3), 644),
-		    new Contributor("Clarissa", "Francesca", titles.apply(3), 645),
-		    new Contributor("Gabby", "Taffy", titles.apply(3), 646)
-		)
-		
-		@BeanProperty var selected = defaultContributor
-
-		val contributorListModel = new BindingListModelList[Contributor](myContributorList)
-		val myTitleListModel = new BindingListModelList[String](titles)
+		val contributorListModel = new BindingListModelList[Contributor](ContributorDataProvider.myContributorList)
+		val myTitleListModel = new BindingListModelList[String](ContributorDataProvider.titles)
 		
 		def allContributors() : BindingListModelList[Contributor] = contributorListModel
 		def titleModel() : BindingListModelList[String] = myTitleListModel
